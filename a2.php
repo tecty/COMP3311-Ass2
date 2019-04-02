c33e96f39130d197beb29d737e6ba7f725713b3a
Toby HUANG <619058900@qq.com>
2019-04-02 21:59:39 +1100

    import the scripts from assignment 1

--- a2.php
+++ a2.php
@@ -2,12 +2,14 @@
 
 // If you want to use the COMP3311 DB Access Library, include the following two lines
 //
-define("LIB_DIR","/import/adams/1/cs3311/public_html/19s1/assignments/a2");
-require_once(LIB_DIR."/db.php");
+// define("LIB_DIR","/import/adams/1/cs3311/public_html/19s1/assignments/a2");
+// require_once(LIB_DIR."/db.php");
+require_once("db.php");
 
 // Your DB connection parameters, e.g., database name
 //
-define("DB_CONNECTION","dbname=a2");
+// define("DB_CONNECTION","dbname=a2");
+define("DB_CONNECTION","dbname=mydb host=localhost user=postgres password=apple123");
 
 //
 // Include your other common PHP code below
